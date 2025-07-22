ae9b5b6d631527c803fd50789e895bfb598dba85
Juli <julik.roy@gmail.com>
2025-07-22 17:38:26 +0530

    add pending.jsx

--- dashboard/src/views/Pending.jsx
+++ dashboard/src/views/Pending.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Pending() {
   return (
-    <div>Pending</div>
-  )
+    <div>
+      <div className=" flex-col p-9  ">
+        <h1 className="font-bold text-2xl" >Pending Your Account</h1>
+        <p className="text-md mt-4 ">
+          Your account is currently pending. Please complete your profile setup
+          to activate your account. Once submitted, your profile will be
+          reviewed and approved by the admin. If you need help, feel free to
+          connect with chat support.
+        </p>
+      </div>
+
+      <div className="flex  pl-9  ">
+        <button className="bg-[#fefeff] p-3 rounded-md border border-[#d2d3d2] text-[#6f6f70] ">
+          <Link to="/seller/dashboard/profile" >
+          Update Profile </Link>
+        </button>
+
+        <button className="bg-[#fefeff] p-3 ml-3 rounded-md border border-[#d2d3d2] text-[#6f6f70] ">
+          <Link to="/seller/dashboard/chatSupport" >Chat Support</Link>
+        </button>
+
+      </div>
+    </div>
+  );
 }
 
-export default Pending
+export default Pending;
